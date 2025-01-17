cdc7f5118d90f10b60b298625d46a9fd503e0fc5
huangbh <herberth.huang@icloud.com>
2025-01-17 22:06:41 +0800

    fix: 更新网站图标配置

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -10,7 +10,12 @@ export default defineConfig({
   lang: "zh-CN",
   title: "herberth",
   description: "herberth 技术博客",
-  head:[['link', { rel: 'icon', href: '/docs/favicon.ico' }]],
+  head:[
+    ['link', { rel: 'icon', href: '/docs/favicon.ico' }],
+    ['link', { rel: 'icon', type:'image/png', size:'16x16', href: '/docs/favicon-16x16.png' }],
+    ['link', { rel: 'icon', type:'image/png', size:'32x32', href: '/docs/favicon-32x32.png' }]
+    ['link', { rel: 'apple-touch-icon', type:'image/png', size:'32x32', href: '/docs/apple-touch-icon.png' }]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav,
