e9d7a1359a21161f4212c43b583cbff3b01b339a
huangbh <herberth.huang@icloud.com>
2025-01-17 22:15:55 +0800

    fix(docs): 修正链接格式问题

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   head:[
     ['link', { rel: 'icon', href: '/docs/favicon.ico' }],
     ['link', { rel: 'icon', type:'image/png', size:'16x16', href: '/docs/favicon-16x16.png' }],
-    ['link', { rel: 'icon', type:'image/png', size:'32x32', href: '/docs/favicon-32x32.png' }]
+    ['link', { rel: 'icon', type:'image/png', size:'32x32', href: '/docs/favicon-32x32.png' }],
     ['link', { rel: 'apple-touch-icon', type:'image/png', size:'32x32', href: '/docs/apple-touch-icon.png' }]
   ],
   themeConfig: {
