7e0d3ff85965e4e8da2e0a2f728d9c7c877179e1
huangbh <herberth.huang@icloud.com>
2025-03-04 22:57:51 +0800

    refactor: 重构侧边栏和工具部分，优化格式和内容

--- docs/.vitepress/config/router.js
+++ docs/.vitepress/config/router.js
@@ -13,188 +13,245 @@
  */
 // 前端
 export const frontend = [
-    {
-        text: "scss", collapsed: true, items: [
-            {
-                text: "scss",
-                link: "/前端/SCSS"
-            }
-        ]
-    }
-]
+  {
+    text: "scss",
+    collapsed: true,
+    items: [
+      {
+        text: "scss",
+        link: "/前端/SCSS",
+      },
+    ],
+  },
+];
 // 后端
 export const backend = [
-    {
-        text: "Linux", collapsed: true,
+  {
+    text: "Linux",
+    collapsed: true,
+    items: [
+      { text: "pgrep", link: "/后端/Linux/pgrep" },
+      { text: "xargs", link: "/后端/Linux/xargs" },
+    ],
+  },
+  {
+    text: "操作系统",
+    collapsed: true,
+    items: [{ text: "GDB", link: "/后端/操作系统/GDB" }],
+  },
+  {
+    text: "数据库",
+    collapsed: true,
+    items: [
+      {
+        text: "oracle",
+        collapsed: true,
         items: [
-            {text: "pgrep", link: "/后端/Linux/pgrep"},
-            {text: "xargs", link: "/后端/Linux/xargs"},
+          { text: "dump", link: "/后端/数据库/oracle/dump命令" },
+          { text: "服务启停", link: "/后端/数据库/oracle/服务启停" },
         ],
-    },
-    {
-        text: "操作系统", collapsed: true, items: [
-            {text: "GDB", link: "/后端/操作系统/GDB"},
-        ]
-    },
-    {
-        text: "数据库", collapsed: true, items: [
-            {
-                text: "oracle", collapsed: true, items: [
-                    {text: "dump", link: "/后端/数据库/oracle/dump命令"},
-                    {text: "服务启停", link: "/后端/数据库/oracle/服务启停"},
-                ]
-            },
-
-        ]
-    },
-    {
-        text: "设计模式", collapsed: true,
+      },
+    ],
+  },
+  {
+    text: "设计模式",
+    collapsed: true,
+    items: [{ text: "SOLID原则", link: "/后端/设计模式/SOLID原则" }],
+  },
+  {
+    text: "Spring Boot",
+    collapsed: true,
+    items: [
+      {
+        text: "基础",
+        collapsed: true,
         items: [
-            {text: "SOLID原则", link: "/后端/设计模式/SOLID原则"},
+          {
+            text: "CommandLineRunner",
+            link: "/后端/Spring Boot/CommandLineRunner",
+          },
+          {
+            text: "BeanPostProcessor",
+            link: "后端/Spring Boot/BeanPostProcessor",
+          },
+          {
+            text: "InitializingBean",
+            link: "后端/Spring Boot/InitializingBean",
+          },
         ],
-    },
-    {
-        text: "Spring Boot", collapsed: true,
+      },
+    ],
+  },
+  {
+    text: "Netty",
+    collapsed: true,
+    items: [
+      {
+        text: "Netty的概念及体系结构",
+        collapsed: true,
         items: [
-            {
-                text: "基础", collapsed: true, items: [
-                    {text: "CommandLineRunner", link: "/后端/Spring Boot/CommandLineRunner"},
-                    {text: "BeanPostProcessor", link: "后端/Spring Boot/BeanPostProcessor"},
-                    {text: "InitializingBean", link: "后端/Spring Boot/InitializingBean"},
-                ]
-            }
-        ]
-    },
-    {
-        text: "Netty", collapsed: true,
+          {
+            text: "第1章 Netty 异步和事件驱动",
+            link: "/后端/Netty/1、Netty异步和事件驱动.md",
+          },
+          {
+            text: "第2章 你的第一个Netty应用程序",
+            link: "/后端/Netty/2、你的第一个Netty应用程序.md",
+          },
+          {
+            text: "第3章 Netty的组件和设计",
+            link: "/后端/Netty/3、Netty的组件和设计.md",
+          },
+          { text: "第4章 传输", link: "/后端/Netty/4、传输.md" },
+          { text: "第5章 ByteBuf", link: "/后端/Netty/5、ByteBuf" },
+          {
+            text: "第6章 ChannelHandler和ChannelPipeline",
+            link: "/后端/Netty/6、ChannelHandler和ChannelPipeline",
+          },
+          {
+            text: "第7章 EventLoop和线程模型",
+            link: "后端/Netty/7、EventLoop和线程模型",
+          },
+          { text: "第8章 引导", link: "/后端/Netty/8、引导" },
+          { text: "第9章 单元测试", link: "/后端/Netty/9、单元测试" },
+        ],
+      },
+      {
+        text: "编解码器",
+        collapsed: true,
         items: [
-            {
-                text: "Netty的概念及体系结构", collapsed: true, items: [
-                    {text: "第1章 Netty 异步和事件驱动", link: "/后端/Netty/1、Netty异步和事件驱动.md"},
-                    {text: "第2章 你的第一个Netty应用程序", link: "/后端/Netty/2、你的第一个Netty应用程序.md"},
-                    {text: "第3章 Netty的组件和设计", link: "/后端/Netty/3、Netty的组件和设计.md"},
-                    {text: "第4章 传输", link: "/后端/Netty/4、传输.md"},
-                    {text: "第5章 ByteBuf", link: "/后端/Netty/5、ByteBuf"},
-                    {
-                        text: "第6章 ChannelHandler和ChannelPipeline",
-                        link: "/后端/Netty/6、ChannelHandler和ChannelPipeline"
-                    },
-                    {text: "第7章 EventLoop和线程模型", link: "后端/Netty/7、EventLoop和线程模型"},
-                    {text: "第8章 引导", link: "/后端/Netty/8、引导"},
-                    {text: "第9章 单元测试", link: "/后端/Netty/9、单元测试"},
-                ]
-            },
-            {
-                text: "编解码器", collapsed: true, items: [
-                    {text: "第10章 编解码器框架", link: "/后端/Netty/10、编解码器.md"},
-                    {
-                        text: "第11章 预置的 ChannelHandler 和编解码器",
-                        link: "/后端/Netty/11、预置的ChannelHandler和编解码器.md"
-                    },
-                ]
-            },
-            {
-                text: "网络协议", collapsed: true, items: [
-                    {text: "第12章 WebSocket", link: "/后端/Netty/13、WebSocket.md"},
-                    {text: "第13章 使用UDP广播事件", link: "/后端/Netty/14、使用UDP广播事件.md"},
-                ]
-            }
-        ]
-    },
-    {
-        text: " Kafka", collapsed: true,
+          { text: "第10章 编解码器框架", link: "/后端/Netty/10、编解码器.md" },
+          {
+            text: "第11章 预置的 ChannelHandler 和编解码器",
+            link: "/后端/Netty/11、预置的ChannelHandler和编解码器.md",
+          },
+        ],
+      },
+      {
+        text: "网络协议",
+        collapsed: true,
         items: [
-            {
-                text: "1、初识Kafka", collapsed: true, items: [
-                    {text: "发布与订阅消息系统", link: ''},
-                    {text: "Kafka登场", link: ''},
-                    {text: "为什么选择Kafka", link: ''},
-                    {text: "数据生态系统", link: ''},
-                    {text: "起源故事", link: ''},
-                ]
-            }
-        ]
-    }
-]
-
+          { text: "第12章 WebSocket", link: "/后端/Netty/12、WebSocket.md" },
+          {
+            text: "第13章 使用UDP广播事件",
+            link: "/后端/Netty/13、使用UDP广播事件.md",
+          },
+        ],
+      },
+      {
+        text: "案例研究",
+        collapsed: true,
+        items: [
+          { text: "第14章 案例研究一", link: "/后端/Netty/14、案例研究一.md" },
+          { text: "第15章 案例研究二", link: "/后端/Netty/15、案例研究二.md" },
+        ],
+      },
+    ],
+  },
+  {
+    text: " Kafka",
+    collapsed: true,
+    items: [
+      {
+        text: "1、初识Kafka",
+        collapsed: true,
+        items: [
+          { text: "发布与订阅消息系统", link: "" },
+          { text: "Kafka登场", link: "" },
+          { text: "为什么选择Kafka", link: "" },
+          { text: "数据生态系统", link: "" },
+          { text: "起源故事", link: "" },
+        ],
+      },
+    ],
+  },
+];
 
 export const tools = [
-    {
-        text: "sketch", collapsed: true,
-        items: [
-            {text: "基本操作", link: "/工具/sketch/基本操作"},
-        ],
-    },
-    {
-        text: "ManimGL", collapsed: true,
-        items: [
-            {text: "manim", link: "/工具/manim/index"},
-        ],
-    },
-    {
-        text: "Vim", collapsed: true, items: [
-            {
-                text: "vim基本使用", link: "/工具/vim/VIM使用"
-            }
-        ],
-    },
-    {
-        text: "typora", collapsed: true, items: [
-            {
-                text: "yaml-front-matter",
-                link: "/工具/typora/yaml-front-matter.md"
-            }
-        ]
-    },
-    {
-        text: "vitepress", collapsed: true, items: [
-            {
-                text: "开始",
-                link: "/工具/vitepress/开始.md"
-            },
-            {
-                text: "Markdown",
-                link: "/工具/vitepress/Markdown.md"
-            },
-            {
-                text: "在Markdown使用Vue",
-                link: "/工具/vitepress/在Markdown使用Vue.md"
-            },
-        ]
-    }
-]
+  {
+    text: "sketch",
+    collapsed: true,
+    items: [{ text: "基本操作", link: "/工具/sketch/基本操作" }],
+  },
+  {
+    text: "ManimGL",
+    collapsed: true,
+    items: [{ text: "manim", link: "/工具/manim/index" }],
+  },
+  {
+    text: "Vim",
+    collapsed: true,
+    items: [
+      {
+        text: "vim基本使用",
+        link: "/工具/vim/VIM使用",
+      },
+    ],
+  },
+  {
+    text: "typora",
+    collapsed: true,
+    items: [
+      {
+        text: "yaml-front-matter",
+        link: "/工具/typora/yaml-front-matter.md",
+      },
+    ],
+  },
+  {
+    text: "vitepress",
+    collapsed: true,
+    items: [
+      {
+        text: "开始",
+        link: "/工具/vitepress/开始.md",
+      },
+      {
+        text: "Markdown",
+        link: "/工具/vitepress/Markdown.md",
+      },
+      {
+        text: "在Markdown使用Vue",
+        link: "/工具/vitepress/在Markdown使用Vue.md",
+      },
+    ],
+  },
+];
 
-export const interest = []
-
+export const interest = [];
 
 // 侧边栏
 export const sidebar = {
-    "/前端/": [{
-        text: "前端",
-        items: frontend
-    }],
-    "/后端/": [
-        {
-            text: "后端",
-            items: backend,
-        },
-    ],
-    "/兴趣/": [{
-        text: "兴趣",
-        items: interest,
-    }],
-    "/工具/": [
-        {
-            text: "工具",
-            items: tools,
-        },
-    ]
-}
+  "/前端/": [
+    {
+      text: "前端",
+      items: frontend,
+    },
+  ],
+  "/后端/": [
+    {
+      text: "后端",
+      items: backend,
+    },
+  ],
+  "/兴趣/": [
+    {
+      text: "兴趣",
+      items: interest,
+    },
+  ],
+  "/工具/": [
+    {
+      text: "工具",
+      items: tools,
+    },
+  ],
+};
 // 顶部导航栏
 export const nav = [
-    {text: "主页", link: "/"},
-    {text: "后端", link: "/后端/index"},
-    {text: "前端", link: "/前端/SCSS"},
-    {text: "兴趣", link: "/兴趣/"},
-    {text: "工具", link: "/工具/index"},
-]
+  { text: "主页", link: "/" },
+  { text: "后端", link: "/后端/index" },
+  { text: "前端", link: "/前端/SCSS" },
+  { text: "兴趣", link: "/兴趣/" },
+  { text: "工具", link: "/工具/index" },
+];
--- docs/后端/Netty/11、预置的ChannelHandler和编解码器.md
+++ docs/后端/Netty/11、预置的ChannelHandler和编解码器.md
@@ -5,9 +5,72 @@ typora-root-url: /Volumes/硬盘/Code/docs/docs/public
 
 ## 通过SSL/TLS保护Netty应用程序
 
+如今, 数据隐私是一个非常值得关注的问题, 作为开发人员, 我们需要准备好应对它。 至少, 我们应该熟悉像SSL和TLS [^1]。 这样的安全协议,它们层叠在其他协议之上,用以实现数据安全。我们在访问安全网站时遇到过这些协议,但是它们也可用于其他不是基于HTTP的应用程序,如安全SMTP(SMTPS)邮件服务器甚至是关系型数据库系统。
+
+为了支持 SSL/TLS,Java 提供了 javax.net.ssl 包,它的 SSLContext 和 SSLEngine 类使得实现解密和加密相当简单直接。Netty 通过一个名为 SslHandler 的 ChannelHandler 实现利用了这个 API,其中 SslHandler 在内部使用 SSLEngine 来完成实际的工作。 
+图 11-1 展示了使用 SslHandler 的数据流
+
+:::tip Netty 的 OpenSSL/SSLEngine 实现
+
+Netty 还提供了使用 OpenSSL 工具包(www.openssl.org)的 SSLEngine 实现。这个 OpenSslEngine 类提供了比 JDK 提供的 SSLEngine 实现更好的性能。 
+
+如果OpenSSL库可用, 可以将 Netty 应用程序 (客户端和服务器) 配置为默认使用OpenSslEngine。
+如果不可用,Netty 将会回退到 JDK 实现。有关配置 OpenSSL 支持的详细说明,参见 Netty 文档: http://netty.io/wiki/forked-tomcat-native.html#wikih2-1。 
+
+注意,无论你使用 JDK 的 SSLEngine 还是使用 Netty 的 OpenSslEngine,SSL API 和数据流都是一致的。
+
+:::
+
+<img src="/Netty实战_page_162_3.png" alt="Netty实战_page_162_3" style="zoom:20%;" />
+
+<TableCaption title='图 11-1  通过 SslHandler 进行解密和加密的数据流' />
+
+代码清单11-1展示了如何使用ChannelInitializer来将SslHandler添加到ChannelPipeline 中。回想一下,ChannelInitializer 用于在 Channel 注册好时设置 ChannelPipeline。
+
+```java [代码清单 11-1  添加 SSL/TLS 支持]
+public class SslChannelInitializer extends ChannelInitializer < Channel > {
+    private final SslContext context;
+    private final boolean startTls;
+    public SslChannelInitializer(SslContext context, boolean startTls) {
+        this.context = context;
+        this.startTls = startTls;
+    }
+    @Override 
+    protected void initChannel(Channel ch) throws Exception {
+        SSLEngine engine = context.newEngine(ch.alloc());
+        ch.pipeline().addFirst("ssl", new SslHandler(engine, startTls));
+    }
+}
+```
+
+在大多数情况下, SslHandler 将是 ChannelPipeline 中的第一个 ChannelHandler。这确保了只有在所有其他的 ChannelHandler 将它们的逻辑应用到数据之后,才会进行加密。  
+
+SslHandler 具有一些有用的方法,如表 11-1 所示。例如,在握手阶段,两个节点将相互验证并且商定一种加密方式。你可以通过配置 SslHandler 来修改它的行为,或者在 SSL/TLS 握手一旦完成之后提供通知,握手阶段完成之后,所有的数据都将会被加密。SSL/TLS 握手将会被自动执行。
+
+<TableCaption title='表 11-1  SslHandler的方法' />
+
+| 方 法 名 称                                                  | 描    述                                                     |
+| ------------------------------------------------------------ | ------------------------------------------------------------ |
+| setHandshakeTimeout (long,TimeUnit)<br/>setHandshakeTimeoutMillis (long)<br/>getHandshakeTimeoutMillis() | 设置和获取超时时间,超时之后,握手ChannelFuture将会被通知失败  |
+| setCloseNotifyTimeout(long,TimeUnit)<br/>setCloseNotifyTimeoutMillis(long)<br/>getCloseNotifyTimeoutMillis() | 设置和获取超时时间,超时之后,将会触发一个关闭通知并关闭连接。 这也将会导致通知该ChannelFuture失败 |
+| handshakeFuture()                                            | 返回一个在握手完成后将会得到通知的ChannelFuture。如果握手先前已经执行过了, 则返回一个包含了先前的握手结果的ChannelFuture |
+| close()<br/>close(ChannelPromise)<br/>close(ChannelHandlerContext,ChannelPromise) | 发送close_notify以请求关闭并销毁底层的SslEngine              |
+
 ## 构建基于Netty的HTTP/HTTPS应用程序
 
+HTTP/HTTPS 是最常见的协议套件之一,并且随着智能手机的成功,它的应用也日益广泛, 因为对于任何公司来说, 拥有一个可以被移动设备访问的网站几乎是必须的。 这些协议也被用于其他方面。许多组织导出的用于和他们的商业合作伙伴通信的 WebService API 一般也是基于HTTP(S)的。 
+
+接下来,我们来看看 Netty 提供的 ChannelHandler,你可以用它来处理 HTTP 和 HTTPS 协议,而不必编写自定义的编解码器。
+
 ### HTTP解码器、编码器和编解码器
+
+<img src="/Netty实战_page_164_1.png" alt="Netty实战_page_164_1" style="zoom:20%;" />
+
+<TableCaption title='图 11-2  HTTP 请求的组成部分' />
+
+<img src="/Netty实战_page_164_2.png" alt="Netty实战_page_164_2" style="zoom:20%;" />
+
+<TableCaption title='图 11-3  HTTP 响应的组成部分' />
 
 ### 聚合HTTP消息
 
@@ -34,3 +97,5 @@ typora-root-url: /Volumes/硬盘/Code/docs/docs/public
 ### 使用JBoss Marshalling进行序列化
 
 ### 通过Protocol Buffers序列化
+
+[^1]:传输层安全(TLS)协议,1.2 版:http://tools.ietf.org/html/rfc5246
